6ee848db8973b8cb9a7fa166f25f4bad7a348676
Jamie Lopez <jamielopez@cpp.edu>
2018-09-30 15:08:38 -0700

    added lambda function in Greeter.java

--- Greeter.java
+++ Greeter.java
@@ -10,5 +10,18 @@ public class Greeter
 		Greeter greeter = new Greeter();
 		HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
 		greeter.greet(helloWorldGreeting);
+		
+		MyLambda myLambdaFunction = () -> System.out.print("Hello World!");
+		MyAdd addFunction = (int a, int b) -> a + b;
 	}
+}
+
+interface MyLambda
+{
+	void foo();
+}
+
+interface MyAdd
+{
+	int add(int x, int y);
 }
