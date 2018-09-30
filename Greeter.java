40dc1e7ff9585ae1a760b1544c2ec9742f144e93
Jamie Lopez <jamielopez@cpp.edu>
2018-09-30 14:50:15 -0700

    added Greeting interface

--- Greeter.java
+++ Greeter.java
@@ -1,2 +1,13 @@
 public class Greeter
 {
+	public void greet()
+	{
+		System.out.print("Hello World");
+	}
+	 
+	public static void main(String[] args)
+	{
+		Greeter greeter = new Greeter();
+		greeter.greet();
+	}
+}
