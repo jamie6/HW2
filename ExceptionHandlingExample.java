e2a2b32b8d687adffd2a83b575ff20a783ade8b6
Jamie Lopez <jamielopez@cpp.edu>
2018-09-30 18:12:25 -0700

    added exceptionhandlingexample.java

---
+++ ExceptionHandlingExample.java
@@ -0,0 +1,20 @@
+// exception handling example
+import java.util.function.BiConsumer;
+public class ExceptionHandlingExample
+{
+	public static void main(String[] args)
+	{
+		int[] someNumbers = { 1, 2, 3, 4 };
+		int key = 2;
+		
+		process(someNumbers, key, (v, k)->System.out.print(v+k));
+	}
+	
+	private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer)
+	{
+		for ( int i: someNumbers )
+		{
+			consumer.accept(i, key);
+		}
+	}
+}
