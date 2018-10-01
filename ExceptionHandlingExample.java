9ce8a648aa7a63ee0bff6759ddddc6b539387099
Jamie Lopez <jamielopez@cpp.edu>
2018-09-30 18:16:36 -0700

    made changes to exceptionhandlingexample

--- ExceptionHandlingExample.java
+++ ExceptionHandlingExample.java
@@ -5,9 +5,19 @@ public class ExceptionHandlingExample
 	public static void main(String[] args)
 	{
 		int[] someNumbers = { 1, 2, 3, 4 };
-		int key = 2;
+		int key = 0;
 		
-		process(someNumbers, key, (v, k)->System.out.print(v+k));
+		process(someNumbers, key, (v, k)->
+		{
+			try
+			{
+				System.out.println(v/k);
+			}
+			catch (ArithmeticException e)
+			{
+				System.out.println("An Arithmetic exception happened");
+			}
+		});
 	}
 	
 	private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer)
