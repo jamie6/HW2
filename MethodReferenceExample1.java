adf02d0b0390d2c6649f25a660c0aeb0a17b9e40
Jamie Lopez <jamielopez@cpp.edu>
2018-09-30 18:44:44 -0700

    added MethodReferenceExample1.java

---
+++ MethodReferenceExample1.java
@@ -0,0 +1,15 @@
+// MethodReferenceExample1
+
+public class MethodReferenceExample1
+{
+	public static void main(String[] args)
+	{
+		Thread t = new Thread(()->printMessage());
+		t.start();
+	}
+	
+	public static void printMessage()
+	{
+		System.out.println("Hello");
+	}
+}
