73257851f0a1494b2402e3ae573b5f53626c8040
Jamie Lopez <jamielopez@cpp.edu>
2018-09-30 18:49:55 -0700

    added MethodReferenceExample2.java

--- MethodReferenceExample1.java
+++ MethodReferenceExample1.java
@@ -4,7 +4,7 @@ public class MethodReferenceExample1
 {
 	public static void main(String[] args)
 	{
-		Thread t = new Thread(()->printMessage());
+		Thread t = new Thread(MethodReferenceExample1::printMessage);
 		t.start();
 	}
 	
