fdf258220281c86090599dc8c9f386457f768538
Jamie Lopez <jamielopez@cpp.edu>
2018-09-30 18:24:14 -0700

    wrapper lambda

--- ExceptionHandlingExample.java
+++ ExceptionHandlingExample.java
@@ -7,17 +7,7 @@ public class ExceptionHandlingExample
 		int[] someNumbers = { 1, 2, 3, 4 };
 		int key = 0;
 		
-		process(someNumbers, key, (v, k)->
-		{
-			try
-			{
-				System.out.println(v/k);
-			}
-			catch (ArithmeticException e)
-			{
-				System.out.println("An Arithmetic exception happened");
-			}
-		});
+		process(someNumbers, key, wrapperLambda((v, k)->System.out.println(v/k)));
 	}
 	
 	private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer)
@@ -26,5 +16,20 @@ public class ExceptionHandlingExample
 		{
 			consumer.accept(i, key);
 		}
+	}
+	
+	private static BiConsumer<Integer, Integer> wrapperLambda(BiConsumer<Integer, Integer>  consumer)
+	{
+		return (v, k)->
+		{
+			try
+			{
+				consumer.accept(v, k);
+			}
+			catch( ArithmeticException e)
+			{
+				System.out.println("Exception caught in wrapper lambda");
+			}
+		};
 	}
 }
