675008d8e30fc17cfe4881a2bb135b57c0a802f0
Jamie Lopez <jamielopez@cpp.edu>
2018-09-30 19:03:36 -0700

    added StreamsExample1.java

---
+++ StreamsExample1.java
@@ -0,0 +1,26 @@
+// StreamsExample1
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.function.Predicate;
+import java.util.function.Consumer;
+
+class StreamsExample1
+{
+	public static void main(String[] args)
+	{
+		List<Person> people = Arrays.asList(
+			new Person("Charles", "Dickens", 60),
+			new Person("Lewis", "Carroll", 42),
+			new Person("Thomas", "Carlyle", 51),
+			new Person("Charlotte", "Bronte", 45),
+			new Person("Matthew", "Arnold", 39)
+		);
+		
+		people.stream()
+		.filter(p-> p.getLastName().startsWith("C"))
+		.forEach(p->System.out.println(p.getFirstName()));
+	}
+}
