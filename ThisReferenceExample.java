088bfcfdd3ee05573e073b9b5b28a55dcbc6fd07
Jamie Lopez <jamielopez@cpp.edu>
2018-09-30 18:41:27 -0700

    made changes to ThisReferenceExample.java

--- ThisReferenceExample.java
+++ ThisReferenceExample.java
@@ -7,22 +7,29 @@ public class ThisReferenceExample
 		p.process(i);
 	}
 	
+	public void execute()
+	{
+		doProcess(10, i->
+		{
+			System.out.println("Value of i is " + i);
+			System.out.println(this);
+		});
+	}
+	
 	public static void main(String[] args)
 	{
 		ThisReferenceExample thisReferenceExample = new ThisReferenceExample();
-		thisReferenceExample.doProcess(10, new Process()
+		/*thisReferenceExample.doProcess(10, i->
 		{
-			@Override
-			public void process(int i )
-			{
-				System.out.println("Value of i is " + i);
-				System.out.println(this);
-			}
-			
-			public String toString()
-			{
-				return "This is the anonymous inner class";
-			}
-		});
+			System.out.println("Value of i is " + i);
+			//System.out.println(this);
+		});*/
+		
+		thisReferenceExample.execute();
+	}
+	
+	public String toString()
+	{
+		return "This is the main ThisReferenceExample class instance";
 	}
 }
