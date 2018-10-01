16fdfcac04b04776421f3d0988b967a86a5850eb
Jamie Lopez <jamielopez@cpp.edu>
2018-09-30 18:35:15 -0700

    added ThisReferenceExample.java

--- ClosuresExample.java
+++ ClosuresExample.java
@@ -6,14 +6,7 @@ public class ClosuresExample
 	{
 		int a = 10;
 		int b = 20;
-		doProcess( a, new Process() 
-		{
-			@Override
-			public void process(int i )
-			{
-				System.out.println(i+b);
-			}
-		});
+		doProcess( a, i->System.out.println(i+b));
 	}
 	
 	public static void doProcess(int i, Process p)
